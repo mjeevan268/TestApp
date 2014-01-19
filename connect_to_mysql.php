fe4647fb68d24b6768ddb0724aa08b62a714bac3
mjeevan268 <mjeevan268@gmail.com>
2014-01-19 11:20:44 +0530

    FirstApp

--- connect_to_mysql.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "aadhaarusers.db.10504428.hostedresource.com";
-$db_name = "aadhaarusers";
-$db_username = "aadhaarusers";
-$db_password = "Aadhaar#123";
-mysql_connect("$db_host","$db_username","$db_password") or die ("could not connect to database");
-mysql_select_db("$db_name") or die ("no database");
-?>
